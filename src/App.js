20fcaef35c1a0835c2cb72c8bd9204cca2bcfcab
YanaSus <susljakovajana@yandex.ru>
2024-02-18 22:38:10 +0300

    Pagination (first part)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/classCounter';
 import './styles/App.css'
@@ -11,24 +11,37 @@ import MySelect from './components/UI/select/MySelect';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import { usePosts } from './hooks/usePosts';
+import axios from 'axios';
+import PostService from './API/PostService';
+import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
+import { getPageCount, getPagesArray } from './utils/pages';
 
 function App() {
 
   const [posts, setPosts] = useState([])
-
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+
+  const [fetchPosts, isPostsLoading,  postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit))
+  })
+
+  let pagesArray = getPagesArray(totalPages);
+
+  useEffect(() => {fetchPosts()}, [])
 
   const createPost = (newPost) =>{
     setPosts([...posts, newPost])
     setModal(false)
   }
-
-  // async function fetchPosts(){
-  //   const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-  //   console.log(response.data)
-  // }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
@@ -36,7 +49,7 @@ function App() {
 
   return (
     <div className="App">
-        {/* <button onClick={fetchPosts}>GET POSTS</button> */}
+        <button onClick={fetchPosts}>GET POSTS</button>
         <MyButton 
           style={{marginTop: '30px'}}
           onClick={() => setModal(true)} 
@@ -54,8 +67,15 @@ function App() {
         filter={filter}
         setFilter={setFilter}
         />
-
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про js"/>
+        {postError &&
+          <h1>Произошла ошибка ${postError}</h1>
+        }
+        
+        {isPostsLoading 
+          ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+          : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про js"/>
+        }
+        
             
         
     </div>
