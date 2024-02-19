a0200244d0cecb960a210fd32540505a180b69de
YanaSus <susljakovajana@yandex.ru>
2024-02-20 01:05:27 +0300

    Dynamic navigation

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useFetching } from '../hooks/useFetching';
+import PostService from '../API/PostService';
+import Loader from '../components/UI/Loader/Loader';
 
 const PostIdPage = () => {
-    const params = useParams();
-    console.log(params)
+
+    const [post, setPost] = useState({});
+
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getById(id);
+        setPost(response.data)
+    })
+
+    useEffect(() => {
+        fetchPostById(params.id);
+    }, [])
+
+    const params = useParams(); 
     return (
         <div>
-            <h1>Вы перешли на страницу поста</h1>
+            <h1>Вы перешли на страницу поста c ID = {params.id}</h1>
+            {isLoading
+                ? <Loader/>
+                : <div>{post.id}. {post.title}</div>
+            }
         </div>
     );
 };
