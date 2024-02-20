6bf5e52d4bd4cdfc685f4985ba83bffd36f9b966
YanaSus <susljakovajana@yandex.ru>
2024-02-20 11:18:45 +0300

    Add comments to the posts

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -16,4 +16,8 @@ export default class PostService {
         return response
         
     }
+    static async getCommentsByPostId(id) {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+        return response
+    }
 }
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,23 +7,43 @@ import Loader from '../components/UI/Loader/Loader';
 const PostIdPage = () => {
 
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+    const params = useParams(); 
 
     const [fetchPostById, isLoading, error] = useFetching(async (id) => {
         const response = await PostService.getById(id);
         setPost(response.data)
     })
 
+    const [fetchComments, isComLoading, errorCom] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(id);
+        setComments(response.data)
+    })
+
     useEffect(() => {
         fetchPostById(params.id);
+        fetchComments(params.id)
     }, [])
 
-    const params = useParams(); 
+    
     return (
         <div>
             <h1>Вы перешли на страницу поста c ID = {params.id}</h1>
             {isLoading
                 ? <Loader/>
                 : <div>{post.id}. {post.title}</div>
+            }
+            <h1>Комментарии</h1>
+            {isComLoading
+                ? <Loader/>
+                : <div>
+                    {comments.map(comm => {
+                    return <div style={{marginTop: 15}}>
+                        <h5>{comm.email}</h5>
+                        <div>{comm.body}</div>
+                    </div>
+                })}
+                </div>
             }
         </div>
     );
